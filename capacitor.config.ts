e82949de0103dde60c666f63b88fafde7b4f0239
Pierrickian <8685110+Pierrickian@users.noreply.github.com>
2026-05-06 00:20:46 +0200

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.eagames.ballrules',
+  appName: 'Ball Rules',
+  webDir: 'artifacts/3d-game/dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
